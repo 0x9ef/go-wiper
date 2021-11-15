fcab259aaf3d6d966bf28a06bb52554eac5ee96e
0x9ef <0x9ef@tutanota.com>
2021-11-15 16:05:57 +0200

    Changed invalid flag (FlagNative) to FlagCrypto to cryptographically strong byte generation

--- wipe/random.go
+++ wipe/random.go
@@ -5,29 +5,27 @@ package wipe
 
 import (
 	crand "crypto/rand"
+	"math/big"
 	rand "math/rand"
 	"time"
 )
 
-// End Of File
 const EOF = 0
 
 // GetRandomByte returns a random byte with selected cryptogrphically mode.
 // If flag is RandomCrypto, we use a crypto/rand module for generation strongly cryprographically byte.
 // In otherwise flags we use a standart cryptographically library
-func GetRandomByte(n int, flag Flag) (byte, error) {
-	if flag&FlagNative != 0 {
-		b := []byte{0}
-		_, err := crand.Reader.Read(b)
+func GetRandomByte(n uint8, flag Flag) (byte, error) {
+	if n == 0 {
+		return EOF, nil
+	}
+	if flag&FlagCrypto != 0 {
+		bigN, err := crand.Int(crand.Reader, big.NewInt(int64(n)))
 		if err != nil {
 			return EOF, err
 		}
-		return b[0], nil
+		return byte(bigN.Int64()), nil
 	}
-
 	rand.Seed(time.Now().UnixNano())
-	if n > 255 {
-		return 0, nil
-	}
-	return byte(rand.Intn(n)), nil
+	return byte(rand.Intn(int(n))), nil
 }
